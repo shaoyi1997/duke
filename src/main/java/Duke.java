981852429f28649ad99105d4ca6985a16c644b5f
shaoyi1997 <e0310084@u.nus.edu>
2019-08-20 17:11:15 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static String insertIndentation(String input) {
+        return "    ____________________________________________________________" + "\n     " + input + "\n"
+                + "    ____________________________________________________________";
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(insertIndentation("Hello! I'm Duke\n" +
+                "     What can I do for you?"));
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(insertIndentation(input));
+            input = sc.nextLine();
+        }
+        if (input.equals("bye")) {
+            System.out.println(insertIndentation("Bye. Hope to see you again soon!"));
+        }
     }
 }
