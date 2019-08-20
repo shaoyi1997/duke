d083806cbe886903fb988dc6c55b2f513564dd4e
shaoyi1997 <e0310084@u.nus.edu>
2019-08-21 00:23:23 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
 
-    private static String insertIndentation(String input) {
-        return "    ____________________________________________________________" + "\n     " + input + "\n"
+    private static String insertLines(String input) {
+        return "    ____________________________________________________________" + "\n" + input + "\n"
                 + "    ____________________________________________________________";
     }
 
-    public static void main(String[] args) {
-        System.out.println(insertIndentation("Hello! I'm Duke\n" +
-                "     What can I do for you?"));
+    private static void run() {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        ArrayList<Task> addedItems = new ArrayList<Task>();
+        int numOfItems = 0;
+        String input;
+
+        System.out.println(insertLines("     Hello! I'm Duke\n" + "     What can I do for you?"));
+        input = sc.nextLine();
+
         while (!input.equals("bye")) {
-            System.out.println(insertIndentation(input));
+            if (input.equals("list")) {
+                StringBuilder stringOfItems = new StringBuilder();
+                for (int i = 0; i < numOfItems; i++) {
+                    stringOfItems.append("     " + (i + 1) + ". " + addedItems.get(i)
+                            + (i == numOfItems - 1 ? "" : "\n"));
+                }
+                System.out.println(insertLines(stringOfItems.toString()));
+            } else {
+                addedItems.add(new Task(input));
+                numOfItems++;
+                System.out.println(insertLines("     added: " + input));
+            }
             input = sc.nextLine();
         }
-        if (input.equals("bye")) {
-            System.out.println(insertIndentation("Bye. Hope to see you again soon!"));
-        }
+
+        System.out.println(insertLines("     Bye. Hope to see you again soon!"));
+    }
+
+    public static void main(String[] args) {
+        run();
     }
 }
