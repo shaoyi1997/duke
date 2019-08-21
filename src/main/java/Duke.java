90081e30b566fb0b03a936b11cb8f9f346bc707f
shaoyi1997 <e0310084@u.nus.edu>
2019-08-22 00:55:08 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,21 +17,46 @@ public class Duke {
         input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                StringBuilder stringOfItems = new StringBuilder();
+
+            // mark as done
+            if (input.length() >= 6 && input.substring(0,4).equals("done")) {
+                try {
+                    int taskNum = Integer.parseInt(input.substring(5));
+                    if (taskNum > numOfItems) {
+                        System.out.println("List only has " + numOfItems + " items.");
+                    } else {
+                        Task curTask = addedItems.get(taskNum - 1);
+                        curTask.markDone();
+                        System.out.println(insertLines("     Nice! I've marked this task as done: \n     " + curTask));
+                    }
+                } catch (NumberFormatException e) {
+                    addedItems.add(new Task(input));
+                    numOfItems++;
+                    System.out.println(insertLines("     added: " + input));
+                }
+            }
+
+            // list added items
+            else if (input.equals("list")) {
+                StringBuilder stringOfItems = new StringBuilder("     Here are the tasks in your list:\n");
                 for (int i = 0; i < numOfItems; i++) {
                     stringOfItems.append("     " + (i + 1) + ". " + addedItems.get(i)
                             + (i == numOfItems - 1 ? "" : "\n"));
                 }
                 System.out.println(insertLines(stringOfItems.toString()));
-            } else {
+            }
+
+            // add item to list
+            else {
                 addedItems.add(new Task(input));
                 numOfItems++;
                 System.out.println(insertLines("     added: " + input));
             }
+
             input = sc.nextLine();
         }
 
+        // bye
         System.out.println(insertLines("     Bye. Hope to see you again soon!"));
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,18 @@
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String des) {
         this.description = des;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
     }
 
     @Override
     public String toString() {
-        return description;
+        return "[" + (isDone ? '\u2713' : '\u2718') + "] " + description;
     }
 }
