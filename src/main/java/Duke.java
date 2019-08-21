34a50f146895518e9facacb1604ec71fcd935d32
shaoyi1997 <e0310084@u.nus.edu>
2019-08-22 02:42:33 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,45 @@ import java.util.*;
 
 public class Duke {
 
+    private static void addTaskToList(ArrayList<Task> list, String input) {
+        Task tk;
+        int idxOfSlash;
+
+        if (input.length() >= 4) {
+
+            try {
+                // add Deadline
+                if (input.length() >= 8 && input.substring(0,8).equals("deadline")) {
+                    idxOfSlash = input.indexOf("/by");
+                    tk = new Deadline(input.substring(9, idxOfSlash - 1), input.substring(idxOfSlash + 4));
+                }
+
+                // add Event
+                else if (input.length() >= 5 && input.substring(0,5).equals("event")) {
+                    idxOfSlash = input.indexOf("/at");
+                    tk = new Event(input.substring(6, idxOfSlash - 1), input.substring(idxOfSlash + 4));
+                }
+
+                // add ToDos
+                else if (input.substring(0,4).equals("todo")) {
+                    tk = new ToDo(input.substring(5));
+                } else {
+                    System.out.println("Invalid Input");
+                    return;
+                }
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("Invalid Input");
+                return;
+            }
+        } else {
+            System.out.println("Invalid Input");
+            return;
+        }
+        list.add(tk);
+        System.out.println(insertLines("     Got it. I've added this task: \n       "
+                + tk + "\n     Now you have " + list.size() + " tasks in the list."));
+    }
+
     private static String insertLines(String input) {
         return "    ____________________________________________________________" + "\n" + input + "\n"
                 + "    ____________________________________________________________";
@@ -10,7 +49,6 @@ public class Duke {
     private static void run() {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> addedItems = new ArrayList<Task>();
-        int numOfItems = 0;
         String input;
 
         System.out.println(insertLines("     Hello! I'm Duke\n" + "     What can I do for you?"));
@@ -22,23 +60,22 @@ public class Duke {
             if (input.length() >= 6 && input.substring(0,4).equals("done")) {
                 try {
                     int taskNum = Integer.parseInt(input.substring(5));
-                    if (taskNum > numOfItems) {
-                        System.out.println("List only has " + numOfItems + " items.");
+                    if (taskNum > addedItems.size()) {
+                        System.out.println("List only has " + addedItems.size() + " items.");
                     } else {
                         Task curTask = addedItems.get(taskNum - 1);
                         curTask.markDone();
                         System.out.println(insertLines("     Nice! I've marked this task as done: \n     " + curTask));
                     }
                 } catch (NumberFormatException e) {
-                    addedItems.add(new Task(input));
-                    numOfItems++;
-                    System.out.println(insertLines("     added: " + input));
+                    addTaskToList(addedItems, input);
                 }
             }
 
             // list added items
             else if (input.equals("list")) {
                 StringBuilder stringOfItems = new StringBuilder("     Here are the tasks in your list:\n");
+                int numOfItems = addedItems.size();
                 for (int i = 0; i < numOfItems; i++) {
                     stringOfItems.append("     " + (i + 1) + ". " + addedItems.get(i)
                             + (i == numOfItems - 1 ? "" : "\n"));
@@ -48,9 +85,7 @@ public class Duke {
 
             // add item to list
             else {
-                addedItems.add(new Task(input));
-                numOfItems++;
-                System.out.println(insertLines("     added: " + input));
+                addTaskToList(addedItems, input);
             }
 
             input = sc.nextLine();
