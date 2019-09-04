5bd2aecbbc27668345b2eb9dd9c82b3f3d4c883d
shaoyi1997 <e0310084@u.nus.edu>
2019-09-04 19:22:49 +0800

    fixed bug where nonsense input with length less than 4 results in error

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -13,9 +13,11 @@ public class Parser {
      * @return the command object given by the input.
      * @throws DukeException thrown when comm
      */
-    public static Command parse(String input) {
+    public static Command parse(String input) throws DukeException {
         if (input.equals("bye")) {
             return new ExitCommand();
+        } else if (input.length() <= 3) {
+            throw new DukeException("I'm sorry, but I don't know what that means :-(");
         } else if (input.substring(0, 4).equals("find")) {
             return new FindCommand(input.substring(5));
         } else if (input.length() >= 6 && input.substring(0,4).equals("done")) {
@@ -41,29 +43,25 @@ public class Parser {
     protected static Task parseAddCommand(String input) throws DukeException, ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HHmm");
         int idxOfSlash;
-        if (input.length() >= 4) {
-            if (input.length() >= 8 && input.substring(0, 8).equals("deadline")) { // add Deadline
-                idxOfSlash = input.indexOf("/by ");
-                if (idxOfSlash == -1 || input.split(" ")[1].contains("/")) {
-                    throw new DukeException("The description of the deadline is incorrect.");
-                }
-                return new Deadline(input.substring(9, idxOfSlash - 1),
-                                    format.format(format.parse(input.substring(idxOfSlash + 4))));
-            } else if (input.length() >= 5 && input.substring(0, 5).equals("event")) { // add Event
-                idxOfSlash = input.indexOf("/at ");
-                if (idxOfSlash == -1 || input.split(" ")[1].contains("/")) {
-                    throw new DukeException("The description of the event is incorrect.");
-                }
-                return new Event(input.substring(6, idxOfSlash - 1),
-                               format.format(format.parse(input.substring(idxOfSlash + 4))));
-            } else if (input.substring(0, 4).equals("todo")) { // add ToDos
-                try {
-                    return new ToDo(input.substring(5));
-                } catch (StringIndexOutOfBoundsException e) {
-                    throw new DukeException("The description of a todo cannot be empty.");
-                }
-            } else {
-                throw new DukeException("I'm sorry, but I don't know what that means :-(");
+        if (input.length() >= 8 && input.substring(0, 8).equals("deadline")) { // add Deadline
+            idxOfSlash = input.indexOf("/by ");
+            if (idxOfSlash == -1 || input.split(" ")[1].contains("/")) {
+                throw new DukeException("The description of the deadline is incorrect.");
+            }
+            return new Deadline(input.substring(9, idxOfSlash - 1),
+                                format.format(format.parse(input.substring(idxOfSlash + 4))));
+        } else if (input.length() >= 5 && input.substring(0, 5).equals("event")) { // add Event
+            idxOfSlash = input.indexOf("/at ");
+            if (idxOfSlash == -1 || input.split(" ")[1].contains("/")) {
+                throw new DukeException("The description of the event is incorrect.");
+            }
+            return new Event(input.substring(6, idxOfSlash - 1),
+                             format.format(format.parse(input.substring(idxOfSlash + 4))));
+        } else if (input.substring(0, 4).equals("todo")) { // add ToDos
+            try {
+                return new ToDo(input.substring(5));
+            } catch (StringIndexOutOfBoundsException e) {
+                throw new DukeException("The description of a todo cannot be empty.");
             }
         } else {
             throw new DukeException("I'm sorry, but I don't know what that means :-(");
